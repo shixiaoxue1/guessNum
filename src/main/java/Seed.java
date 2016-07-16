49a56ce4dc861812e0c5fe2f33c30f760fc72bc7
shixiaoxue <shixiaoxue@bit.edu.cn>
2016-07-16 14:51:49 +0800

    change

--- src/main/java/Seed.java
+++ src/main/java/Seed.java
@@ -5,7 +5,7 @@ import java.util.StringJoiner;
  * Created by twer on 7/9/16.
  */
 public class Seed implements ISeed{
-    @Override
+   
     public String generate() {
         Random random = new Random();
         return "" + random.nextInt(10) +random.nextInt(10) +random.nextInt(10) +random.nextInt(10);
